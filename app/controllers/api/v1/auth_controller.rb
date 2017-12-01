e04a432b2e77d6079bd81be9074dac78d31fbb73
kylewstewart <kyle.w.stewart@mac.com>
2017-11-30 21:59:10 -0500

    Seed Bug Fix

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -9,7 +9,6 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def create
-    binding.pry
     user = User.find_by(username: params[:username])
     if user.present? && user.authenticate(params[:password])
       render json: {
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "test", password_digest: "password")
+User.create(username: "test", password: "password")
