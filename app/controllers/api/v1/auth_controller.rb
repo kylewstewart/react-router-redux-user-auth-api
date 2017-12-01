2fb59ab5fa70d53c6ba10ccbcc66ab355b51897a
kylewstewart <kyle.w.stewart@mac.com>
2017-11-30 20:17:56 -0500

    bug fixes

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def create
+    binding.pry
     user = User.find_by(user_name: params[:user_name])
     if user.present? && user.authenticate(params[:password])
       render json: {
@@ -16,7 +17,6 @@ class Api::V1::AuthController < ApplicationController
         user_name: user.user_name,
         jwt: JWT.encode({user_id: user.id}, nil, 'none')
         }
-      }
     else
       render json: {
         eror: 'User Name or Password is Incorrect'
