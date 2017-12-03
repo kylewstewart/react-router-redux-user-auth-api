5dd65e3020179a427b71f10753a8880b169e013a
kylewstewart <kyle.w.stewart@mac.com>
2017-12-02 23:41:28 -0500

    bug fix

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::AuthController < ApplicationController
         }
     else
       render json: {
-        eror: 'User Name or Password is Incorrect'
+        error: 'User Name or Password is Incorrect'
       }, status: 404
     end
   end
